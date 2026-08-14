c2139295fc9a44e653f7f10e973fbc9f9b16cee3
jrob6716 <121855892+jrob6716@users.noreply.github.com>
2026-08-14 00:49:22 -0700

    Add latest catalog tracks

--- js/catalog-data.js
+++ js/catalog-data.js
@@ -8,6 +8,7 @@ window.musicCatalog = [
             'Wish you were here.mp3',
             'Love somebody Acoustic Master.m4a',
             'Ride.mp3',
+            'Lose a Lover.mp3',
             'Tribalskunk.mp3'
         ]
     },
@@ -37,7 +38,7 @@ window.musicCatalog = [
             'Roscoes Frestyle.wav',
             'Island Fever.wav',
             'Illdrumz2.mp3',
-            'da joint.MP3',
+            'da joint.mp3',
             'Better Daze.mp3',
             'church jam.MP3',
             'Mitsubishi .mp3',
@@ -58,7 +59,8 @@ window.musicCatalog = [
             'Patience_V2D.mp3',
             'Eyes.mp3',
             'Breathless.mp3',
-            'Physical_Attraction.mp3'
+            'Physical_Attraction.mp3',
+            'WAKE ME DA JR.m4a'
         ]
     },
     {
@@ -66,7 +68,11 @@ window.musicCatalog = [
         files: [
             'Dreamers unafraid.mp3',
             'Lost with you .mp3',
-            'Tell Me .mp3'
+            'Tell Me .mp3',
+            'PARACHUTE DA JR.m4a',
+            'OPEN HEART JR .m4a',
+            'FOSTER M83 JR.m4a',
+            'BIG POP DRUMS JR .m4a'
         ]
     },
     {
@@ -82,28 +88,31 @@ window.musicCatalog = [
     {
         category: 'Dance & Club',
         files: [
-            'Jason Robinson - The Last Sunrise.mp3',
             '10 Hilary Duff - Anyday ( Co-Produced for Richard Vission ).mp3',
+            '01 DEMO PREVIEW  -  Dirtyfreqs & Vassy - Turn up the Party.mp3',
             '7 Eva Simons - Silly Boy ( Co-Produced for Richard Vission ).mp3',
             'LONDON ROSE KICKDRUM IKON FINAL RADIO.m4a',
             '8 Pussycat Dolls - Bottle Pop ( Co-Produced for Richard Vission ).mp3',
+            'Big Fun.m4a',
             '4 U2 - Magnificent - ( Co-Produced for Richard Vission ).mp3',
             'Jason Robinson - Avanced Stimulation.mp3',
+            'IN MY UNIVERSE.m4a',
             '11 Morning After - Richard Vission Remix.mp3',
-            'Jason Robinson - Stigmata.mp3',
+            'Dirtyfreqs + Vassy - T.U.T.P (Turn up th party) 4.5 EDIT.m4a',
             '3 Hilary Duff - Reach Out ( Co-Produced for Richard Vission ).mp3',
             '6 Will.I.am - Get Ya Money ( Co-Produced for Richard Vission ).mp3',
+            'Dirtyfreqs + Vassy - T.U.T.P (Turn up th party) 4.5 EDIT 1.m4a',
+            'THE MOMENT.m4a',
             '2 Katy Perry - Kissed A Girl ( Co-Produced for Richard Vission ).mp3',
-            'Forever young  2025 NEW .mp3',
-            'Jason Robinson - Stresspill.mp3',
+            'In My Dreams.m4a',
             'Jason Robinson - Dark Mind.mp3',
             '1 Weezer - Trouble Maker ( Co-Produced for Richard Vission ).mp3',
+            'Forever young.mp3',
             '5 David May - Super Star ( Co-Produced for Richard Vission ).mp3',
             'Without you.mp3',
-            'Jason Robinson - TunnelVision.mp3',
-            'Turn Up The Bass (Ikon Original).mp3',
-            '9 One Republic - Apologize ( Co-Produced for Richard Vission ).mp3',
-            'Jason Robinson - Chemical Hysteria.mp3'
+            'JC Like this Final MSTR 0517 0828.mp3',
+            'Turn Up The Bass.mp3',
+            '9 One Republic - Apologize ( Co-Produced for Richard Vission ).mp3'
         ]
     },
     {
